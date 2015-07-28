6da522289c4577c9c41121d42fc39c489caa6fea
Alvaro del Castillo <acs@bitergia.com>
2015-07-28 20:04:18 +0200

    Added first tests using protractor. Just testing title and number of categories.

--- html/e2e-tests/protractor.conf.js
+++ html/e2e-tests/protractor.conf.js
@@ -1,19 +1,5 @@
+// conf.js
 exports.config = {
-  allScriptsTimeout: 11000,
-
-  specs: [
-    '*.js'
-  ],
-
-  capabilities: {
-    'browserName': 'chrome'
-  },
-
-  baseUrl: 'http://localhost:8000/app/',
-
-  framework: 'jasmine',
-
-  jasmineNodeOpts: {
-    defaultTimeoutInterval: 30000
-  }
-};
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['scenarios.js']
+}
--- html/e2e-tests/scenarios.js
+++ html/e2e-tests/scenarios.js
@@ -1,42 +1,22 @@
-'use strict';
+// spec.js
+describe('Scout timeline app', function() {
 
-/* https://github.com/angular/protractor/blob/master/docs/toc.md */
-
-describe('my app', function() {
-
-  browser.get('index.html');
-
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
-  });
-
-
-  describe('view1', function() {
+    var numCategories = 2;
 
     beforeEach(function() {
-      browser.get('index.html#/view1');
+        browser.get('http://compose_scout_1');
+        // browser.get('http://sega.bitergia.net:9090');
     });
 
-
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
+    it('should have a title', function() {
+        expect(browser.getTitle()).toEqual('Scout timeline');
     });
 
-  });
-
-
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#/view2');
+    it('Categories loaded', function() {
+        // browser.pause();
+        element(by.model('category')).evaluate('categories.length').then(
+                function(count) {
+                    expect(count).toEqual(numCategories);
+                });
     });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
-    });
-
-  });
 });
