af85a5b28f5215199d567f904e09ae78dcfb12b7
Alvaro del Castillo <acs@bitergia.com>
2015-07-28 20:50:53 +0200

    Added more data tests. Just first steps.

--- html/e2e-tests/scenarios.js
+++ html/e2e-tests/scenarios.js
@@ -2,10 +2,16 @@
 describe('Scout timeline app', function() {
 
     var numCategories = 2;
+    var events_limit = 10;
+    var numDataSources = 5;
+    var pageSize = 30; // used in infinite scrolling
+    var scope; // angular main scope
+
 
     beforeEach(function() {
         browser.get('http://compose_scout_1');
         // browser.get('http://sega.bitergia.net:9090');
+        scope = element(by.model('category'));
     });
 
     it('should have a title', function() {
@@ -14,9 +20,26 @@ describe('Scout timeline app', function() {
 
     it('Categories loaded', function() {
         // browser.pause();
-        element(by.model('category')).evaluate('categories.length').then(
+        scope.evaluate('categories.length').then(
                 function(count) {
                     expect(count).toEqual(numCategories);
                 });
     });
+
+    it('Data sources loaded', function() {
+        scope.evaluate('categories[0].backends.length').then(
+                function(count) {
+                    expect(count).toEqual(numDataSources);
+                });
+    });
+
+    it('Scout event timeline built', function() {
+        // browser.pause();
+        scope.evaluate('scout_events.length').then(
+                function(count) {
+                    expect(count).toEqual(pageSize);
+                });
+    });
+
+
 });
