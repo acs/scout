325f38e7069bf8ba916e692b5c8fdafc46d5b83a
Alvaro del Castillo <acs@bitergia.com>
2015-09-11 06:51:30 +0200

    Make logs dir configurable using command line param

--- utils/update-scout.sh
+++ utils/update-scout.sh
@@ -1,6 +1,11 @@
 export PATH=$PATH:/usr/local/bin
 SCOUT_HOME=/home/bitergia/scout
 LOGS=$SCOUT_HOME/logs
+
+if [ "$#" -eq 1 ]
+    then LOGS=$1
+fi
+
 mkdir -p $LOGS
 cd $SCOUT_HOME
-make clean newevents deploy 2>&1 | cat > $LOGS/scout.log-`date +%s`
+make newevents deploy 2>&1 | cat > $LOGS/scout.log-`date +%s`
